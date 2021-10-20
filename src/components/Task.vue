284af4022648cf5e6dbc47e22745aa391452e8b8
nadirbasalamah <nadir008basalamah@gmail.com>
2021-10-20 16:14:11 +0700

    feat(app): Adding toggle reminder. Menambahkan fitur untuk toggle reminder.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,11 @@
 <template>
-  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
     <h3>
       {{ task.text }}
-      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -14,11 +17,6 @@ export default {
   props: {
     task: Object,
   },
-  methods: {
-    onDelete(id) {
-      this.$emit("delete-task", id);
-    },
-  },
 };
 </script>
 
@@ -26,15 +24,18 @@ export default {
 .fas {
   color: red;
 }
+
 .task {
   background: #f4f4f4;
   margin: 5px;
   padding: 10px 20px;
   cursor: pointer;
 }
+
 .task.reminder {
   border-left: 5px solid green;
 }
+
 .task h3 {
   display: flex;
   align-items: center;
